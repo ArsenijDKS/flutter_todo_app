cf9a807a87aa1c03fa45700af6bc84ab689ec683
Arsenij Kudravcev <ars.manag.gg@gmail.com>
2023-05-31 19:51:13 +0300

    Added tiles and some simple logic

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/utilities/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +9,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // List of todo tasks
+  List toDoList = [
+    ["make a dinner", false],
+    ["exercise", false],
+  ];
+
+  // Checkbox was tapped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.green[300],
+      appBar: AppBar(
+        title: Text("TO DO"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
+      ),
+    );
   }
 }
---
+++ lib/utilities/todo_tile.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ToDoTile extends StatelessWidget {
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+
+  ToDoTile(
+      {super.key,
+      required this.taskName,
+      required this.taskCompleted,
+      required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
+      child: Container(
+        padding: EdgeInsets.all(25.0),
+        decoration: BoxDecoration(
+            color: Colors.green, borderRadius: BorderRadius.circular(10)),
+        child: Row(
+          children: [
+            // Checkbox
+            Checkbox(
+              value: taskCompleted,
+              onChanged: onChanged,
+              activeColor: Colors.grey[800],
+            ),
+
+            // Task text
+            Text(
+              taskName,
+              style: TextStyle(
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
